03006c2ab3b8979d33385ea1eda0a631fbd7574c
somramnani <som.ramnani@gmail.com>
2023-03-27 17:55:11 -0400

    add vercel.json & settings

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get('/api/:nft', async (req, res) => {
- await axios({
-    headers: {'Content-Type': 'application/json'},
-    method: 'post',
+app.get("/api/:nft", async (req, res) => {
+  await axios({
+    headers: { "Content-Type": "application/json" },
+    method: "post",
     url: "https://api.primitives.xyz/api/interview/searchTokens",
     data: {
-      query: req.params.nft
-    }
-  }).then(function(response){
-    res.json(response.data)
-  })
-})
+      query: req.params.nft,
+    },
+  }).then(function (response) {
+    res.json(response.data);
+  });
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+module.exports = app;
