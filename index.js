12d402c6c2cf2f3488b1b98ba24ac8d231979c70
somramnani <som.ramnani@gmail.com>
2023-04-11 15:37:00 -0400

    edit vercel.json

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ app.get("/api/get/*", async (req, res) => {
 });
 
 app.get("/api/post/*/:data", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader("Access-Control-Allow-Headers", "content-type");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
   const urlLink = req.params[0];
   const authHeaders = req.headers.authorization || null;
   const data = req.params.data;
