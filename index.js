f66394a80c1662db7d376dc2d36c387d573b9167
somramnani <som.ramnani@gmail.com>
2024-11-15 17:01:23 -0500

    fix(server): add declaration of app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const app = express();
 
 const corsOptions = {
   origin: "http://localhost:3000",
