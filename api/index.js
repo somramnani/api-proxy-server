c24b7f10a16e8bafbad9eedc7a0fbc3e3cd9a690
somramnani <som.ramnani@gmail.com>
2024-11-15 18:16:16 -0500

    fix(deployment-issue): add a new index.js file

--- api/index.js
+++ api/index.js
@@ -1,68 +1,5 @@
 const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
 const app = express();
-
-app.use(cors());
-
-app.get("/api/get/*", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow certain methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  const urlLink = req.params[0];
-  const authHeaders = req.headers.authorization || null;
-
-  await axios({
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authHeaders,
-    },
-    method: "get",
-    url: urlLink,
-  })
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log("error", err);
-    });
-  console.log("params:", req.params);
-});
-
-app.get("/api/post/*/:data", async (req, res) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Max-Age", "1800");
-  // res.setHeader("Access-Control-Allow-Headers", "content-type");
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  // );
-  const urlLink = req.params[0];
-  const authHeaders = req.headers.authorization || null;
-  const data = req.params.data;
-
-  await axios({
-    headers: {
-      "Content-Type": "application/json",
-      accept: "application/json",
-      Authorization: authHeaders,
-      "Access-Control-Allow-Origin": "*",
-    },
-    method: "post",
-    url: urlLink,
-    data: {
-      query: data,
-    },
-  })
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log("error", err);
-    });
-  console.log(" Post params:", req.params);
-});
 
 app.get("/", (req, res) => {
   const hotText = "repo";
@@ -74,8 +11,6 @@ app.get("/", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
