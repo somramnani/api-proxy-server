bbe2b7dec26ac63b26af5c347e0e8071d60d1468
somramnani <som.ramnani@gmail.com>
2024-11-20 15:27:06 -0500

    chore(codecov-installation): change server

--- api/tests/hello.test.js
+++ api/tests/hello.test.js
@@ -1,10 +1,23 @@
 const request = require("supertest");
 const app = require("../index");
-const server = require("../server");
+// const server = require("../server");
+
+// afterAll(async () => {
+//   console.log("Closing server...");
+//   await new Promise((resolve) => server.close(resolve));
+// });
+
+let server;
+
+beforeAll(async () => {
+  server = app.listen(3001, () => {
+    console.log("Server started on port 3001");
+  });
+});
 
 afterAll(async () => {
   console.log("Closing server...");
-  await new Promise((resolve) => server.close(resolve));
+  await new Promise((resolve) => server.close(resolve)); // Wait for the server to shut down
 });
 
 describe("GET /hello", () => {
--- api/tests/index.test.js
+++ api/tests/index.test.js
@@ -1,12 +1,23 @@
 const request = require("supertest");
 const app = require("../index");
-const server = require("../server");
+// const server = require("../server");
+
+// afterAll(async () => {
+//   console.log("Closing server...");
+//   await new Promise((resolve) => server.close(resolve));
+// });
+let server;
+
+beforeAll(async () => {
+  server = app.listen(3001, () => {
+    console.log("Server started on port 3001");
+  });
+});
 
 afterAll(async () => {
   console.log("Closing server...");
-  await new Promise((resolve) => server.close(resolve));
+  await new Promise((resolve) => server.close(resolve)); // Wait for the server to shut down
 });
-
 describe("GET /", () => {
   it("should return a welcome message with a link to the documentation", async () => {
     const response = await request(server).get("/");
